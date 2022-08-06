156c33f9f496771da34cecb35f68298fa80fe2b3
Ray Martino <raymardesign@gmail.com>
2022-08-06 12:26:40 -0400

    Routing

--- apps/sprint-naming/src/app/app.module.ts
+++ apps/sprint-naming/src/app/app.module.ts
@@ -1,14 +1,52 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './header/header.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SuggestionListComponent } from './suggestion-list/suggestion-list.component';
+import { VoteComponent } from './vote/vote.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SuggestionListComponent,
+    VoteComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    NavigationComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: 'list', component: SuggestionListComponent },
+      { path: 'nav', component: NavigationComponent },
+      { path: 'vote', component: VoteComponent },
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+      { path: '**', component: PageNotFoundComponent },
+    ]),
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ apps/sprint-naming/src/app/suggestion-list/suggestion-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'raymar-suggestion-list',
+  templateUrl: './suggestion-list.component.html',
+  styleUrls: ['./suggestion-list.component.scss'],
+})
+export class SuggestionListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
